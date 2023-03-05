7bfc48803e7b7e34e881e60091cfaae0e8446d03
Nafets94 <45308745+Nafets94@users.noreply.github.com>
2023-03-06 00:19:44 +0200

    added an AppBar

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,9 @@
-import { Typography } from "@mui/material";
+import { CssBaseline } from "@mui/material";
+import { Container } from "@mui/system";
 import { useEffect, useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
 import { Product } from "../models/product";
+import Header from "./Header";
 
 function App() {
 
@@ -15,8 +17,11 @@ function App() {
 
   return (
     <>
-      <Typography variant="h1">Re-Store</Typography>
-      <Catalog products={products} />
+      <CssBaseline />
+      <Header />
+      <Container>
+        <Catalog products={products} />
+      </Container>
     </>
   );
 }
