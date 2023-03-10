a54595afc663271693a45136ee8de9b916e0ea7d
Nafets94 <45308745+Nafets94@users.noreply.github.com>
2023-03-10 21:54:26 +0200

    handling validation errors

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,7 +1,17 @@
-import { Button, ButtonGroup, Container, Typography } from "@mui/material";
+import { Alert, AlertTitle, Button, ButtonGroup, Container, List, ListItem, ListItemText, Typography } from "@mui/material";
+import { useState } from "react";
 import agent from "../../app/api/agent";
 
 export default function AboutPage() {
+
+    const [validationErrors, setValidationErrors] = useState<string[]>([]);
+
+    function getValidationError() {
+        agent.TestErrors.getValidationError()
+            .then(() => console.log('should not see this'))
+            .catch(error => setValidationErrors(error));
+    }
+
     return (
         <Container>
             <Typography gutterBottom variant='h2'>Errors for testing purposes</Typography>
@@ -28,10 +38,22 @@ export default function AboutPage() {
                 </Button>
                 <Button
                     variant="contained"
-                    onClick={() => agent.TestErrors.getValidationError().catch(error => console.log(error))}>
+                    onClick={getValidationError}>
                         Test Validation Error
                 </Button>
             </ButtonGroup>
+            {validationErrors.length > 0 &&
+                <Alert severity="error">
+                    <AlertTitle>ValidationErrors</AlertTitle>
+                    <List>
+                        {validationErrors.map(error => (
+                            <ListItem key={error}>
+                                <ListItemText>{error}</ListItemText>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Alert>
+            }
         </Container>
     )
 }
