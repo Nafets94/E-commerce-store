38cad7568b86234e2368dc48cf679b4a9b363748
Nafets94 <45308745+Nafets94@users.noreply.github.com>
2023-03-11 21:41:56 +0200

    added a server error component

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -4,6 +4,7 @@ import Catalog from "../../features/catalog/Catalog";
 import ProductDetails from "../../features/catalog/ProductDetails";
 import ContactPage from "../../features/contact/ContactPage";
 import HomePage from "../../features/home/HomePage";
+import ServerError from "../errors/ServerError";
 import App from "../layout/App";
 
 export const router = createBrowserRouter([
@@ -15,7 +16,8 @@ export const router = createBrowserRouter([
             {path: 'catalog', element: <Catalog />},
             {path: 'catalog/:id', element: <ProductDetails />},
             {path: 'about', element: <AboutPage />},
-            {path: 'contact', element: <ContactPage />}
+            {path: 'contact', element: <ContactPage />},
+            {path: 'server-error', element: <ServerError />},
         ]
     }
 ])
