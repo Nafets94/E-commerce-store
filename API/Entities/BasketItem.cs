273bfdbd21c480b628c0fb04de51d847d20ed198
Nafets94 <45308745+Nafets94@users.noreply.github.com>
2023-03-13 20:42:54 +0200

    added ef relationships

--- API/Entities/BasketItem.cs
+++ API/Entities/BasketItem.cs
@@ -1,5 +1,8 @@
-﻿namespace API.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
 {
+    [Table("BasketItems")]
     public class BasketItem
     {
         public int Id { get; set; }
@@ -10,5 +13,9 @@
         public int ProductId { get; set; }
 
         public Product Product { get; set; }
+
+        public int BasketId { get; set; }
+
+        public Basket Basket { get; set; }
     }
 }
