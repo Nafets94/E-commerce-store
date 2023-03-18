e133e68ce7587c3fce0cef9056703d1341e18c8d
Nafets94 <45308745+Nafets94@users.noreply.github.com>
2023-03-18 20:03:02 +0200

    fetching the basket on app start

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,13 +1,30 @@
 import { createTheme, CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { Container } from "@mui/system";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Header from "./Header";
 import 'react-toastify/dist/ReactToastify.css';
+import { useStoreContext } from "../context/StoreContext";
+import { getCookie } from "../util/util";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 function App() {
+  const {setBasket} = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookie('buyerId');
+    if (buyerId) {
+      agent.Basket.get()
+      .then(basket => setBasket(basket))
+      .catch(error => console.log(error))
+      .finally(() => setLoading(false));
+    }
+  }, [setBasket])
+
   const [darkMode, setDarkMode] = useState(true);
 
   const paletteType = darkMode ? 'dark' : 'light';
@@ -25,6 +42,8 @@ function App() {
     setDarkMode(darkMode => !darkMode);
   }
 
+  if (loading) return <LoadingComponent message="Initialising app..." />
+ 
   return (
     <ThemeProvider theme={theme}>
       <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
