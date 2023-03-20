fcd1c52f9593c96e696603fead3ffd8dbd3b810e
Nafets94 <45308745+Nafets94@users.noreply.github.com>
2023-03-20 13:33:17 +0200

    installing and testing redux

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,9 +1,17 @@
 import { Typography } from "@mui/material";
+import { useSelector } from "react-redux/es/exports";
+import { CounterState } from "./counterReducer";
 
 export default function ContactPage() {
+    const { title, data } = useSelector((state: CounterState) => state);
     return (
-        <Typography variant="h2">
-            Contact page
-        </Typography>
+        <>
+            <Typography variant="h2">
+                {title}
+            </Typography>
+            <Typography variant="h5">
+                The data is: {data}
+            </Typography>
+        </>
     )
 }
