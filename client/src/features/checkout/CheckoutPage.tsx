b3667bc159f074afa7377128e5cf78e2878da80c
Nafets94 <45308745+Nafets94@users.noreply.github.com>
2023-05-09 22:29:44 +0200

    added a custom text input

--- client/src/features/checkout/CheckoutPage.tsx
+++ client/src/features/checkout/CheckoutPage.tsx
@@ -1,17 +1,11 @@
 import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Toolbar from '@mui/material/Toolbar';
 import Paper from '@mui/material/Paper';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
-import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AddressForm from './AddressForm';
 import PaymentForm from './PaymentForm';
 import Review from './Review';
@@ -30,8 +24,6 @@ function getStepContent(step: number) {
       throw new Error('Unknown step');
   }
 }
-
-const theme = createTheme();
 
 export default function CheckoutPage() {
   const [activeStep, setActiveStep] = React.useState(0);
