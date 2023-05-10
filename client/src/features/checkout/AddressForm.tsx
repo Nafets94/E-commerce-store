b2851541048606ca39e26f0b82398b0a59c554c0
Nafets94 <45308745+Nafets94@users.noreply.github.com>
2023-05-10 19:04:03 +0200

    added a reusable check box

--- client/src/features/checkout/AddressForm.tsx
+++ client/src/features/checkout/AddressForm.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import { useFormContext } from 'react-hook-form';
 import AppTextInput from '../../app/components/AppTextInput';
+import AppCheckbox from '../../app/components/AppCheckbox';
 
 export default function AddressForm() {
   const { control } = useFormContext();
@@ -65,10 +64,7 @@ export default function AddressForm() {
             />
           </Grid>
           <Grid item xs={12}>
-            <FormControlLabel
-              control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-              label="Use this address for payment details"
-            />
+            <AppCheckbox name='saveAddress' label='Save this as the default address' control={control} />
           </Grid>
         </Grid>
     </React.Fragment>
