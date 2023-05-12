673cef8e738a200cacea1dbbe16277f0e6987eeb
Nafets94 <45308745+Nafets94@users.noreply.github.com>
2023-05-12 15:46:49 +0200

    added order submission

--- client/src/features/checkout/CheckoutPage.tsx
+++ client/src/features/checkout/CheckoutPage.tsx
@@ -12,88 +12,109 @@ import Review from './Review';
 import { FieldValues, FormProvider, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { validationSchema } from './checkoutValidation';
+import { useState } from 'react';
+import agent from '../../app/api/agent';
+import { useAppDispatch } from '../../app/store/configureStore';
+import { clearBasket } from '../basket/basketSlice';
+import { LoadingButton } from '@mui/lab';
 
 const steps = ['Shipping address', 'Payment details', 'Review your order'];
 
 function getStepContent(step: number) {
-  switch (step) {
-    case 0:
-      return <AddressForm />;
-    case 1:
-      return <PaymentForm />;
-    case 2:
-      return <Review />;
-    default:
-      throw new Error('Unknown step');
-  }
+	switch (step) {
+		case 0:
+			return <AddressForm />;
+		case 1:
+			return <PaymentForm />;
+		case 2:
+			return <Review />;
+		default:
+			throw new Error('Unknown step');
+	}
 }
 
 export default function CheckoutPage() {
-  const [activeStep, setActiveStep] = React.useState(0);
+	const [activeStep, setActiveStep] = useState(0);
+	const [orderNumber, setOrderNumber] = useState(0);
+	const [loading, setLoading] = useState(false);
+	const dispatch = useAppDispatch();
 
-  const currentValidationSchema = validationSchema[activeStep];
+	const currentValidationSchema = validationSchema[activeStep];
 
-  const methods = useForm({
-    mode: 'onTouched',
-    resolver: yupResolver(currentValidationSchema)
-  });
+	const methods = useForm({
+		mode: 'onTouched',
+		resolver: yupResolver(currentValidationSchema)
+	});
 
-  const handleNext = (data: FieldValues) => {
-    if (activeStep === 2) {
-      console.log(data);
-    }
-    setActiveStep(activeStep + 1);
-  };
+	const handleNext = async (data: FieldValues) => {
+		const { nameOnCard, saveAddress, ...shippingAddress } = data;
+		if (activeStep === steps.length - 1) {
+			setLoading(true);
+			try {
+				const orderNumber = await agent.Orders.create({saveAddress, shippingAddress});
+				setOrderNumber(orderNumber);
+				setActiveStep(activeStep + 1);
+				dispatch(clearBasket());
+				setLoading(false);
+			} catch (error) {
+				console.log(error);
+				setLoading(false);
+			}
+		} else {
+			setActiveStep(activeStep + 1);
+		}
+	};
 
-  const handleBack = () => {
-    setActiveStep(activeStep - 1);
-  };
+	const handleBack = () => {
+		setActiveStep(activeStep - 1);
+	};
 
-  return (
-    <FormProvider {...methods}>
-      <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-        <Typography component="h1" variant="h4" align="center">
-          Checkout
-        </Typography>
-        <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
-          {steps.map((label) => (
-            <Step key={label}>
-              <StepLabel>{label}</StepLabel>
-            </Step>
-          ))}
-        </Stepper>
-        {activeStep === steps.length ? (
-          <React.Fragment>
-            <Typography variant="h5" gutterBottom>
-              Thank you for your order.
-            </Typography>
-            <Typography variant="subtitle1">
-              Your order number is #2001539. We have emailed your order
-              confirmation, and will send you an update when your order has
-              shipped.
-            </Typography>
-          </React.Fragment>
-        ) : (
-          <form onSubmit={methods.handleSubmit(handleNext)}>
-            {getStepContent(activeStep)}
-            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-              {activeStep !== 0 && (
-                <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
-                  Back
-                </Button>
-              )}
-              <Button
-                disabled={!methods.formState.isValid}
-                variant="contained"
-                type='submit'
-                sx={{ mt: 3, ml: 1 }}
-              >
-                {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
-              </Button>
-            </Box>
-          </form>
-        )}
-      </Paper>
-    </FormProvider>
-  );
+	return (
+		<FormProvider {...methods}>
+			<Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+				<Typography component="h1" variant="h4" align="center">
+					Checkout
+				</Typography>
+				<Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+					{steps.map((label) => (
+						<Step key={label}>
+							<StepLabel>{label}</StepLabel>
+						</Step>
+					))}
+				</Stepper>
+				{activeStep === steps.length ? (
+					<React.Fragment>
+						<Typography variant="h5" gutterBottom>
+							Thank you for your order.
+						</Typography>
+						<Typography variant="subtitle1">
+							Your order number is #{orderNumber}. We have not emailed your order
+							confirmation, and will not send you an update when your order has
+							shipped as this is a fake store!
+						</Typography>
+					</React.Fragment>
+				) : (
+					<form onSubmit={methods.handleSubmit(handleNext)}>
+						{getStepContent(activeStep)}
+						<Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+							{activeStep !== 0 && (
+								<Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+									Back
+								</Button>
+							)}
+							<LoadingButton
+								loading={loading}
+								disabled={!methods.formState.isValid}
+								variant="contained"
+								type='submit'
+								sx={{ mt: 3, ml: 1 }}
+							>
+								{activeStep === steps.length - 1 ? 'Place order' : 'Next'}
+							</LoadingButton>
+						</Box>
+					</form>
+				)}
+			</Paper>
+		</FormProvider>
+	);
 }
