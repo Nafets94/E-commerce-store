798b0af5d4d20f8e83c4853a03c39d467ae49fb4
Nafets94 <45308745+Nafets94@users.noreply.github.com>
2023-05-18 15:39:18 +0200

    added stripe payment inputs

--- client/src/features/checkout/PaymentForm.tsx
+++ client/src/features/checkout/PaymentForm.tsx
@@ -6,56 +6,71 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import { useFormContext } from 'react-hook-form';
 import AppTextInput from '../../app/components/AppTextInput';
+import { CardCvcElement, CardExpiryElement, CardNumberElement } from '@stripe/react-stripe-js';
+import { StripeInput } from './StripeInput';
 
 export default function PaymentForm() {
-  const {control} = useFormContext();
+	const { control } = useFormContext();
 
-  return (
-    <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        Payment method
-      </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <AppTextInput name='nameOnCard' label='Name on card' control={control} />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            id="cardNumber"
-            label="Card number"
-            fullWidth
-            autoComplete="cc-number"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            //required
-            id="expDate"
-            label="Expiry date"
-            fullWidth
-            autoComplete="cc-exp"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            //required
-            id="cvv"
-            label="CVV"
-            helperText="Last three digits on signature strip"
-            fullWidth
-            autoComplete="cc-csc"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-            label="Remember credit card details for next time"
-          />
-        </Grid>
-      </Grid>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Typography variant="h6" gutterBottom>
+				Payment method
+			</Typography>
+			<Grid container spacing={3}>
+				<Grid item xs={12} md={6}>
+					<AppTextInput name='nameOnCard' label='Name on card' control={control} />
+				</Grid>
+				<Grid item xs={12} md={6}>
+					<TextField
+						id="cardNumber"
+						label="Card number"
+						fullWidth
+						autoComplete="cc-number"
+						variant="outlined"
+						InputLabelProps={{ shrink: true }}
+						InputProps={{
+							inputComponent: StripeInput,
+							inputProps: {
+								component: CardNumberElement
+							}
+						}}
+					/>
+				</Grid>
+				<Grid item xs={12} md={6}>
+					<TextField
+						id="expDate"
+						label="Expiry date"
+						fullWidth
+						autoComplete="cc-exp"
+						variant="outlined"
+						InputLabelProps={{ shrink: true }}
+						InputProps={{
+							inputComponent: StripeInput,
+							inputProps: {
+								component: CardExpiryElement
+							}
+						}}
+					/>
+				</Grid>
+				<Grid item xs={12} md={6}>
+					<TextField
+						id="cvv"
+						label="CVV"
+						helperText="Last three digits on signature strip"
+						fullWidth
+						autoComplete="cc-csc"
+						variant="outlined"
+						InputLabelProps={{ shrink: true }}
+						InputProps={{
+							inputComponent: StripeInput,
+							inputProps: {
+								component: CardCvcElement
+							}
+						}}
+					/>
+				</Grid>
+			</Grid>
+		</React.Fragment>
+	);
 }
