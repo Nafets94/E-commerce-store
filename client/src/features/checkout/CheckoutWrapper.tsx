da9f18e3072846d235ad53c3efc82d43bb6cb513
Nafets94 <45308745+Nafets94@users.noreply.github.com>
2023-05-18 19:19:12 +0200

    added LoadingComponent

--- client/src/features/checkout/CheckoutWrapper.tsx
+++ client/src/features/checkout/CheckoutWrapper.tsx
@@ -5,6 +5,7 @@ import { useAppDispatch } from "../../app/store/configureStore";
 import { useEffect, useState } from "react";
 import agent from "../../app/api/agent";
 import { setBasket } from "../basket/basketSlice";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 
 const stripePromise = loadStripe('pk_test_51N82MGJ7Px4ryVzGyuA0gcN0osrJSMdgkdoZYjuJ23Fe64JHqXLJbZ8it1zf94SwDmb3UPyOsBka6XWbG7bN3dJl00rwFmfj41')
 
@@ -18,6 +19,8 @@ export default function CheckoutWrapper() {
             .catch(error => console.log(error))
             .finally(() => setLoading(false));
     }, [dispatch]);
+
+    if (loading) return <LoadingComponent message='Loading checkout...' />
 
     return (
         <Elements stripe={stripePromise}>
